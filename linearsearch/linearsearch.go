05c20d0ff298302cf618fd2de9fd83b5421a4ff5
jdev <josh@joshdevelops.com>
2023-11-12 20:31:28 -0600

    made func body empty in linearsearch

--- linearsearch/linearsearch.go
+++ linearsearch/linearsearch.go
@@ -1,10 +1,5 @@
 package linearsearch
 
 func linearSearch(haystack []int, needle int) bool {
-    for _, b := range haystack {
-        if b == needle {
-            return true
-        }
-    }
-    return false
+
 }
